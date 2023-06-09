1d94a4a89d833576b99acb4428d697cc3647e031
leevi <leevi@atguigu.com>
2023-06-09 14:50:14 +0800

    创建单元测试类GitTest

---
+++ crud-common/src/test/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * 包名:com.atguigu
+ *
+ * @author Leevi
+ * 日期2023-06-09  14:49
+ */
+public class GitTest {
+    public void test01(){
+        System.out.println("hello world");
+    }
+}
